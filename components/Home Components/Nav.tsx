3f936560e5c852728344780ba5df970b1449a25b
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-21 10:41:32 +0800

    initial commit

--- components/Home Components/Nav.tsx
+++ components/Home Components/Nav.tsx
@@ -7,8 +7,8 @@ import Link from "next/link"
 type Props = {}
 function Nav({}: Props) {
   return (
-    <main className="  ">
-    <div className=" m-auto  lg:w-[1200px] lg:p-10 md:p-8  p-4 flex items-center justify-between">
+    <main className="shadow-md  ">
+    <div className="  m-auto  lg:w-[1200px] lg:p-10 md:p-8  p-4 flex items-center justify-between">
         <div className=" flex ">
 
         <Image src={logo} alt="Carlos Castro Logo" width={200} height={200} className="  w-[120px] lg:w-[200px] lg:mr-20" />
@@ -18,9 +18,9 @@ function Nav({}: Props) {
             <Bars3Icon className=" lg:hidden w-6" />
             <div className=" hidden lg:flex space-x-10">
 
-        <Link href="/">About</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">Blog</Link>
+        <Link href="/" className=" text-gray-500 hover:text-black transition-all ease-linear duration-150">About</Link>
+        <Link href="/" className=" text-gray-500 hover:text-black transition-all ease-linear duration-150">Contact</Link>
+        <Link href="/" className=" text-gray-500 hover:text-black transition-all ease-linear duration-150">Blog</Link>
         </div>
 
         </div>
