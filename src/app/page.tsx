9923cbadf02f5479b0d3e0eeb239e582c90ce171
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-21 14:37:30 +0800

    services section update - service box design is complete now need to add the real details animation with transition added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Nav from '../../components/Home Components/Nav'
 import Hero from '../../components/Home Components/Hero'
+import Services from '../../components/Home Components/Services'
 
 export default function Home() {
   return (
     <main>
       <Nav />
       <Hero />
+      <Services  />
     </main>
   )
 }
