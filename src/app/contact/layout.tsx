63116e843bd4ebee994795e849f568fbf7b7b900
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-25 16:59:11 +0800

    contact form on working

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -1,29 +1,32 @@
-import Nav from '../../../components/Home Components/Nav'
-import './../globals.css'
+import Nav from "../../../components/Home Components/Nav";
+import "./../globals.css";
 
-import { Poppins } from 'next/font/google'
+import { Poppins } from "next/font/google";
 
 const inter = Poppins({
-  weight: ['100','200','300','400','500','600','700','800','900'],
-   subsets: ['latin'] })
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Carlos Castro Migration',
-  description: ' Carlos Castro Migration and Educatoin Services',
-}
+  title: "Carlos Castro Migration",
+  description: " Carlos Castro Migration and Educatoin Services",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      
       <body className={inter.className}>
+        <div>
+        <Nav />
 
-      <Nav />{children}
+        </div>
+        {children}
       </body>
     </html>
-  )
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,38 @@
-type Props = {}
+import Image from "next/image";
+import bg from "../../../assests/Contact Background.jpg";
+
+type Props = {};
 function Contact({}: Props) {
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="  overflow-hidden w-full h-[280px]  border relative">
+        <div className=" space-y-2 lg:space-y-4 text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20">
+          <h1 className=" font-bold text-3xl md:text-4xl lg:text-5xl text-center text-orange z-30">
+            Contact
+          </h1>
+          <h2 className=" text-2xl text-white font-semibold md:text-xl ">
+            It's your turn to change your{" "}
+            <span className=" text-orange">future.</span>{" "}
+          </h2>
+        </div>
+
+        <Image
+          src={bg}
+          alt="Contact Background Image"
+          width={640}
+          height={247}
+          className="  w-full h-full object-cover"
+        />
+        <span className=" bg-[#201c2c] h-full w-full top-0  left-0 bg-opacity-90 absolute "></span>
+      </div>
+
+{/** CONTACT FORM */}
+      <div>
+        <p className=" uppercase text-orange">How Can i help you</p>
+        <h2 className=" font-bold text-2xl"> Write a Message</h2>
+
+      </div>
+    </div>
+  );
 }
-export default Contact
+export default Contact;
