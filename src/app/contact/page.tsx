92f33c75adfd4b88a2eace82c475b7833bc3cde9
MingmaTenzing <pasangsherpa689@gmail.com>
2023-06-26 17:14:30 +0800

    form work in progress

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -26,12 +26,41 @@ function Contact({}: Props) {
         <span className=" bg-[#201c2c] h-full w-full top-0  left-0 bg-opacity-90 absolute "></span>
       </div>
 
-{/** CONTACT FORM */}
-      <div>
-        <p className=" uppercase text-orange">How Can i help you</p>
-        <h2 className=" font-bold text-2xl"> Write a Message</h2>
-
+      {/** CONTACT FORM */}
+      <div className=" p-4 mt-4">
+        <div className=" space-y-3">
+          <p className=" text-sm font-semi uppercase text-orange">
+            How Can i help you
+          </p>
+          <h2 className=" font-bold text-3xl"> Write a Message</h2>
+        </div>
       </div>
+      <form className="  space-y-4  flex flex-col justify-center items-center ">
+        <div className=" md:flex md:space-y-0 md:space-x-2 space-y-3">
+          <div>
+            <input
+              className="  h-[60px]  p-2  min-w-[300px] outline-none bg-gray-100 rounded-lg "
+              type="text"
+              required
+              placeholder="Your Name"
+            ></input>
+          </div>
+          <div>
+            <input
+              className="  h-[60px] p-2   min-w-[300px] outline-none bg-gray-100 rounded-lg "
+              type="email"
+              required
+              placeholder="Your Eamil Address"
+            ></input>
+          </div>
+        </div>
+        <div>
+          <textarea placeholder=" Enter your Message" className=" p-2  md:w-[602px] bg-gray-100 outline-none   min-w-[300px] rounded-lg h-[300px]" />
+        </div>
+        <button className=" font-semibold bg-orange text-white p-4 rounded-lg px-6 hover:scale-105 transition-all ease-linear duration-150 hover:shadow-lg ">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
