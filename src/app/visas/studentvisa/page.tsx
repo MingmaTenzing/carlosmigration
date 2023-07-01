507c41294267c2065b83994101ddc8a9114fce17
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-01 12:02:21 +0800

    visa navigation block complete

--- src/app/visas/studentvisa/page.tsx
+++ src/app/visas/studentvisa/page.tsx
@@ -1,17 +1,150 @@
-import Head from "next/head"
+"use client";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import Head from "next/head";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-type Props = {}
+type Props = {};
 function StudentVisa({}: Props) {
+  const path = usePathname();
+  useEffect(() => {
+    if (path === "/visas/studentvisa") {
+      console.log("works");
+    }
+  }, []);
+
   return (
-    <div>
+    <div className=" p-4">
+      <div>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/studentvisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Student Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
 
-        <Head>
-           <title>Student Visa</title> 
-        </Head>
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/studentvisa" ? "bg-orange   w-full" : " "
+            }`}
+          ></span>
+        </Link>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/skilledvisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Skilled Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
 
-    <p></p>    
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/skilledvisa" ? "bg-orange   w-full" : " "
+            }`}
+          ></span>
+        </Link>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/familyvisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Family Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
+
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/familyvisa" ? "bg-orange   w-full" : " "
+            }`}
+          ></span>
+        </Link>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/employersponsoredvisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Employer Sponsored Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
+
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/employersponsoredvisa"
+                ? "bg-orange   w-full"
+                : " "
+            }`}
+          ></span>
+        </Link>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/businessvisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Business Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
+
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/businessvisa" ? "bg-orange   w-full" : " "
+            }`}
+          ></span>
+        </Link>
+        <Link
+          href="/"
+          className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
+        >
+          <h2
+            className={` z-10 ${
+              path == "/visas/othervisa" ? "text-white" : " "
+            }`}
+          >
+            {" "}
+            Other Visa{" "}
+          </h2>
+          <ChevronRightIcon className=" w-5 z-10" />
+
+          <span
+            className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
+              path == "/visas/othervisa" ? "bg-orange   w-full" : " "
+            }`}
+          ></span>
+        </Link>
+      </div>
+
+      <div>
+        <p>pathname : {path}</p>
+      </div>
     </div>
-    
-  )
+  );
 }
-export default StudentVisa
+export default StudentVisa;
