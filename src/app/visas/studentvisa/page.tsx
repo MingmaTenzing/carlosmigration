cdfb19f31c35861b081ee449e374e994e231ecc2
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-01 13:50:12 +0800

    the visa navigation box is now made a component

--- src/app/visas/studentvisa/page.tsx
+++ src/app/visas/studentvisa/page.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import students from "../../../../assests/Students Background Image.jpg";
 import bg from "../../../../assests/Visa Background.jpg";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
+import VisaNavigation from "../../../../utilities/VisaNavigation";
 type Props = {};
 function StudentVisa({}: Props) {
   const path = usePathname();
@@ -41,130 +42,8 @@ function StudentVisa({}: Props) {
       {/** BODY */}
 
       <div className=" lg:flex lg:space-x-10 lg:justify-center md:p-8 p-4">
-        <div className="  lg:w-[320px] ">
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/studentvisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Student Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/studentvisa" ? "bg-orange   w-full" : " "
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/skilledvisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Skilled Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/skilledvisa" ? "bg-orange   w-full" : " "
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/familyvisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Family Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/familyvisa" ? "bg-orange   w-full" : " "
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/employersponsoredvisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Employer Sponsored Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/employersponsoredvisa"
-                  ? "bg-orange   w-full"
-                  : " "
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/businessvisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Business Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/businessvisa" ? "bg-orange   w-full" : " "
-              }`}
-            ></span>
-          </Link>
-          <Link
-            href="/"
-            className=" h-[60px] group  justify-between relative hover:text-white  bg-gray-100 p-2 flex items-center "
-          >
-            <h2
-              className={` z-10 ${
-                path == "/visas/othervisa" ? "text-white" : " "
-              }`}
-            >
-              {" "}
-              Other Visa{" "}
-            </h2>
-            <ChevronRightIcon className=" w-5 z-10" />
-
-            <span
-              className={`left-0  group-hover:right-0 w-0 h-full group-hover:w-full transition-all ease-linear duration-300  absolute   bg-orange ${
-                path == "/visas/othervisa" ? "bg-orange   w-full" : " "
-              }`}
-            ></span>
-          </Link>
-        </div>
+      {/** NAVIGATION BOX */}
+      <VisaNavigation />
 
         {/** VISA INFORMATION BLOCK */}
         <div className=" lg:w-1/2 space-y-5 mt-10 lg:mt-0 ">
