f93c79b08f4a5064370c807326c90aa533bec289
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-12 14:22:08 +0800

    fixed layout error demo showcase

---
+++ src/app/services/layout.tsx
@@ -0,0 +1,22 @@
+
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Services",
+  description: "Best Migration Services across Australia."
+ 
+}
+
+export default function ServicesLayout({
+    children, // will be a page or nested layout
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        {/* Include shared UI here e.g. a header or sidebar */}
+     
+        {children}
+      </section>
+    )
+  }
