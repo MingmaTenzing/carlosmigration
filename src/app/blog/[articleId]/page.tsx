8e759f73c78dad1321037f061c14adcb63a30e9f
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-20 15:16:49 +0800

    reverted the changes

--- src/app/blog/[articleId]/page.tsx
+++ src/app/blog/[articleId]/page.tsx
@@ -3,7 +3,7 @@ import { useParams } from "next/navigation";
 import { supabase } from "../../../../components/supabase/supabaseclient";
 import { useEffect, useState } from "react";
 import { Database } from "../../../../supabase";
-
+import Image from "next/image";
 
 interface article {
   article_img: string | null;
@@ -23,26 +23,46 @@ async function getArticle(route: string) {
     .eq("id", route);
   return data;
 }
+
 function BlogPost() {
-  const [post, setPost] = useState<article>();
   const path = useParams();
   const route: string = path.articleId;
+  const [post, setPost] = useState<article>();
+  const date = new Date(post?.created_at!);
+  const fromattedDAted = date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  });
+
+  console.log(post);
 
   useEffect(() => {
     async function fetchArticle() {
       const article = await getArticle(route);
-      if (article) {
-        setPost(article[0]);
-      }
+      setPost(article![0]);
     }
-
     fetchArticle();
   }, []);
 
-  return <div>
-    {
-      post?.author
-    }
-  </div>;
+  return (
+    <div className=" p-4">
+      {/** ARTICLE section */}
+      <div>
+        <Image
+          src={post?.article_img!}
+          alt={post?.title!}
+          width={800}
+          height={500}
+        />
+        <div>
+          <div className=" flex space-x-2 items-center text-gray-500">
+            <p>Author: {post?.author}</p>
+            <p>{fromattedDAted}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default BlogPost;
