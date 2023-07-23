7e8f41dbc3e69bc56c05cd2b2ab08447b402100f
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-23 15:24:46 +0800

    loading state added in the blog section and on the recent blog component

--- src/app/blog/[articleTitle]/[articleId]/page.tsx
+++ src/app/blog/[articleTitle]/[articleId]/page.tsx
@@ -34,6 +34,7 @@ function BlogPost() {
   const route: string = useParams().articleId;
 
   const [post, setPost] = useState<article>();
+  const [loading, setLoading] = useState<boolean>(false)
   const date = new Date(post?.created_at!);
   const fromattedDAted = date.toLocaleDateString("en-US", {
     year: "numeric",
@@ -42,71 +43,109 @@ function BlogPost() {
   });
 
   useEffect(() => {
+    setLoading(true);
     async function fetchArticle() {
       const article = await getArticle(route);
-      setPost(article![0]);
+      if (article) {
+        setPost(article[0]);
+        setLoading(false);
+      }
     }
     fetchArticle();
   }, []);
 
   return (
     <div className=" lg:flex lg:w-[1200px] m-auto lg:space-x-10  p-4">
-      {/** ARTICLE section */}
-      <div className=" space-y-6 md:w-[80%] m-auto lg:w-[750px]   ">
-        <Image
-          src={post?.article_img!}
-          alt={post?.title!}
-          width={800}
-          height={500}
-          className=" max-h-[500px] object-cover object-center rounded-lg"
-        />
 
-        <div className=" space-y-3">
-          <h1 className="  font-bold text-2xl md:text-3xl lg:text-4xl">
-            {post?.title}
-          </h1>
-
-          <h2 className="  italic font-light">{post?.subtitle}</h2>
+      {
+        loading? (      <div className=" animate-pulse md:w-[80%] m-auto  ">
+        <div className="w-full md:w-[full] m-auto h-[300px] lg:h-[400px] bg-gray-500 rounded-lg"></div>
+       <div className=" space-y-4 mt-4">
+        <div className=" w-full h-4 bg-gray-400"></div>
+        <div className=" w-[90%] h-3 bg-gray-400"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
+        <div className=" w-[80%%] h-2 bg-gray-300"></div>
         </div>
-        <div className=" flex  items-center  justify-between  ">
-          <div className=" flex  space-x-2 items-center">
-            <Image
-              src={post?.author_img!}
-              alt={post?.author!}
-              width={200}
-              height={200}
-              className="  w-[50px] h-[50px] rounded-full object-center object-cover"
-            />
-
-            <p> {post?.author}</p>
-          </div>
-          <p className=" text-sm  text-gray-400 italic">{fromattedDAted}</p>
-        </div>
-
-        <div className=" space-y-5">
-          {post?.para?.map((content, index) => (
-            <p key={index}>{content} </p>
-          ))}
-        </div>
-        {post?.youtube_url && (
-          <div>
-            <div className=" md:hidden">
-              <YouTubeEmbed
-                height={300}
-                url={post.youtube_url}
-                width={320}
-              />
-            </div>
-            <div className=" hidden md:flex">
-              <YouTubeEmbed
-                height={450}
-                url={post.youtube_url}
-                width={650}
-              />
-            </div>
-          </div>
-        )}
       </div>
+) : (  
+<div className=" space-y-6 md:w-[80%] m-auto lg:w-[750px]   ">
+  <Image
+    src={post?.article_img!}
+    alt={post?.title!}
+    width={800}
+    height={500}
+    className=" max-h-[500px] object-cover object-center rounded-lg"
+  />
+
+  <div className=" space-y-3">
+    <h1 className="  font-bold text-2xl md:text-3xl lg:text-4xl">
+      {post?.title}
+    </h1>
+
+    <h2 className="  italic font-light">{post?.subtitle}</h2>
+  </div>
+  <div className=" flex  items-center  justify-between  ">
+    <div className=" flex  space-x-2 items-center">
+      <Image
+        src={post?.author_img!}
+        alt={post?.author!}
+        width={200}
+        height={200}
+        className="  w-[50px] h-[50px] rounded-full object-center object-cover"
+      />
+
+      <p> {post?.author}</p>
+    </div>
+    <p className=" text-sm  text-gray-400 italic">{fromattedDAted}</p>
+  </div>
+
+  <div className=" space-y-5">
+    {post?.para?.map((content, index) => (
+      <p key={index}>{content} </p>
+    ))}
+  </div>
+  {post?.youtube_url && (
+    <div>
+      <div className=" md:hidden">
+        <YouTubeEmbed height={300} url={post.youtube_url} width={320} />
+      </div>
+      <div className=" hidden md:flex">
+        <YouTubeEmbed height={450} url={post.youtube_url} width={650} />
+      </div>
+    </div>
+  )}
+</div>)
+      }
+
+
+
+
+
+
+
+
+    
 
       {/*** SIDE BAR OPTIONS */}
       <div className=" mt-10 lg:mt-0 w-full lg:w-[300px] space-y-6">
--- utilities/RecentBlogs.tsx
+++ utilities/RecentBlogs.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import { supabase } from "../components/supabase/supabaseclient";
 import { Blog } from "@/app/blog/page";
@@ -11,53 +11,72 @@ type Props = {
 
 function RecentBlogs({ articleToNotInclude }: Props) {
   const [recentArticles, setRecentArticles] = useState<Blog[]>();
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
+  const loadingArray: number[] = new Array(5).fill(0);
 
   useEffect(() => {
+   
 
     async function getRecentArticles() {
+      setLoading(true);
       const { data: Blogs, error } = await supabase
         .from("Blogs")
         .select("*")
         .neq("id", articleToNotInclude)
         .range(0, 4);
       setRecentArticles(Blogs!);
-     
+      setLoading(false);
     }
-  
+
     getRecentArticles();
-    
-  }, [articleToNotInclude])
+  }, [articleToNotInclude]);
 
   return (
     <div className=" bg-[#f1f0f7] p-8 border rounded-lg space-y-6 lg:w-[300px]">
       <h2 className=" font-bold text-lg">Recent Blogs</h2>
-      {recentArticles?.map((article) => (
-        <div
-          key={article.id}
-          onClick={() => router.push(`/blog/${article.title}/${article.id}`)}
-          className=" cursor-pointer overflow-hidden"
-        >
-          <div className=" flex items-center space-x-2">
-            <Image
-              src={article.article_img!}
-              alt={article.title!}
-              width={100}
-              height={100}
-              className=" rounded-lg w-24 h-20 object-cover"
-            />
-            <div className=" ">
-              <p className=" text-[12px] text-[#73727c]">
-                {" "}
-                By: {article.author}
-              </p>
-              <h3 className="  hover:text-orange truncate font-semibold text-[#1b182f] text-sm">
-                {article.title}
-              </h3>
+      {loading ? (
+        loadingArray.map((_, index) => (
+          <div key={index} className="  animate-pulse flex space-x-3 items-center">
+            <div className=" w-24 h-20 bg-gray-600 rounded-lg" />
+            <div className=" w-1/2 space-y-2">
+              <div className=" w-[full] h-2 bg-gray-400"></div>
+              <div className=" w-full h-4 bg-gray-300"></div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>
+          {recentArticles?.map((article) => (
+            <div
+              key={article.id}
+              onClick={() =>
+                router.push(`/blog/${article.title}/${article.id}`)
+              }
+              className=" cursor-pointer overflow-hidden"
+            >
+              <div className=" flex items-center space-x-2">
+                <Image
+                  src={article.article_img!}
+                  alt={article.title!}
+                  width={100}
+                  height={100}
+                  className=" rounded-lg w-24 h-20 object-cover"
+                />
+                <div className=" ">
+                  <p className=" text-[12px] text-[#73727c]">
+                    {" "}
+                    By: {article.author}
+                  </p>
+                  <h3 className="  hover:text-orange truncate font-semibold text-[#1b182f] text-sm">
+                    {article.title}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ))}{" "}
+        </>
+      )}
     </div>
   );
 }
