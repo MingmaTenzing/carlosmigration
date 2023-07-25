e890179ab6bac2c090fddad5eb98dd187496cd71
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-25 14:52:02 +0800

    testing opengraph

--- src/app/blog/[articleTitle]/[articleId]/opengraph-image.tsx
+++ src/app/blog/[articleTitle]/[articleId]/opengraph-image.tsx
@@ -2,7 +2,9 @@ import { ImageResponse } from "next/server";
 import { supabase } from "../../../../../components/supabase/supabaseclient";
 import { Blog } from "../../page";
 
+
 export const runtime = "edge";
+export const contentType = "image/png";
 
 export const size = {
   width: 1200,
@@ -18,10 +20,19 @@ export default async function Image({ params }: Props) {
     .select("*")
     .eq("id", params.articleID);
 
+    
+    if (data) {
+        const article = data[0];
+        return new ImageResponse(
+            <img src={article.article_img!} alt={article.title!} />
+        )
+    }
+
+
   return new ImageResponse(
     (
-      <div>
-        <img src={data![0].article_img!} alt={data![0].title!} />
+      <div className=" flex justify-center bg-black">
+        <div className=" text-white font-bold">TESTING IN THE WORLD OF DARK</div>
       </div>
     )
   );
