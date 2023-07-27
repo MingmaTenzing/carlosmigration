b4ccbe7ab2174f37898ea3a05d3a9b6a4a2869b9
MingmaTenzing <pasangsherpa689@gmail.com>
2023-07-27 12:05:58 +0800

    dynamic opengraph working now

--- src/app/blog/[articleTitle]/[articleId]/opengraph-image.tsx
+++ src/app/blog/[articleTitle]/[articleId]/opengraph-image.tsx
@@ -19,31 +19,15 @@ type Props = {
 
 // Image generation
 export default async function Image({ params }: Props) {
-  const removeSpaces = unescape(params.articleTitle);
-
- 
-
-
-
   return new ImageResponse(
     (
       // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
 
       <div tw="   relative z-100 flex w-full h-full justify-between items-center bg-[#fefefe] ">
-        <div tw="flex w-1/2 h-full items-center pl-10">
-          <img
-            tw="  w-[500px] absolute  left-0  z-10 "
-            src="https://byeilculfqugwxetgrkj.supabase.co/storage/v1/object/public/article%20images/public/map.png"
-          />
-          <h1  tw="text-[40px] font-bold  z-20">
-          {removeSpaces}
-          </h1>
-        </div>
-
-        <img
-          tw=" h-[400px] absolute  right-10 p-4 rounded-full"
-          src="https://byeilculfqugwxetgrkj.supabase.co/storage/v1/object/public/article%20images/authors/Carlos%20Castro%20Profile.jpg"
+        <img tw=""
+          src={`https://byeilculfqugwxetgrkj.supabase.co/storage/v1/object/public/article%20images/public/${params.articleTitle}`}
         />
+        <div tw="flex absolute bottom-0 right-0  bg-white text-black text-4xl p-4 font-bold">Carlos Castro Migration</div>
       </div>
     )
   );
