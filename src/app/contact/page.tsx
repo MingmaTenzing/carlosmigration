0d0af65e567cf64e4ab7d4323f5c45cfcda622be
MingmaTenzing <pasangsherpa689@gmail.com>
2023-08-17 12:21:31 +0800

    changes made environment variables

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -23,8 +23,8 @@ function Contact({}: Props) {
 
     emailjs
       .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_KEY!,
-        process.env.NEXT_PUBLIC_TEMPLATE_KEY!,
+        process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID!,
+        process.env. NEXT_PUBLIC_EMAIL_TEMPLATE_ID!,
         form.current!,
         "5lDQ2fsinXAaz-OgN"
       )
